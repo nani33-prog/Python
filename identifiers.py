b04328e1f2262a173391906d2fc4a96365148636
nani33-prog <nanifreefire33@gmail.com>
2026-07-27 12:44:19 +0530

    Delete identifiers.py

--- identifiers.py
+++
@@ -1,8 +0,0 @@
-student = "Nani Babu"
-_student_id = "25341A05P8"
-STUDENT_ACADEMIC_YEAR = "2025-2029"
-SubjectName = "Python Programming"
-def student_sem():
-    print("3rd Semester")
-print(student, _student_id, STUDENT_ACADEMIC_YEAR, SubjectName)
-student_sem()
