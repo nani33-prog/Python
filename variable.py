5e350a59b08ea13b1f56f11fc848b2a786c9188d
nani33-prog <nanifreefire33@gmail.com>
2026-07-27 22:21:20 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,14 @@
+name = "V,Nani Babu"
+age = 18
+height =5.4
+is_student = True
+print("Name ",name)
+print("Age is ",age)
+print("Height is ",height)
+print("Student -> ",is_student)
+
+#OUTPUT
+#Name  V,Nani Babu
+#Age is  18
+#Height is  5.4
+#Student ->  True
