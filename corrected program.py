b3d4d12352737134bb5ad630b795d2f005f0278c
nani33-prog <nanifreefire33@gmail.com>
2026-07-29 23:17:27 +0530

    Add files via upload

---
+++ corrected program.py
@@ -0,0 +1,9 @@
+#corrected program
+x=10
+if x > 0 :
+ print("positive")
+else :
+ print("non-positive")
+
+#output
+#positive
---
+++ dynamic typing.py
@@ -0,0 +1,13 @@
+#dynamic typing
+value = 100
+print(value)
+print(type(value))
+value = "Python"
+print(value)
+print(type(value))
+
+#output
+#100
+#<class 'int'>
+#Python
+#<class 'str'>
