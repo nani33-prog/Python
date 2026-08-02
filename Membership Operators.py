2bbb7f4e89d8e3e9fe675d887410b7b68a936e37
nani33-prog <nanifreefire33@gmail.com>
2026-08-02 14:00:04 +0530

    Add files via upload

---
+++ Membership Operators.py
@@ -0,0 +1,14 @@
+fruits = ["apple", "banana", "mango", "grape", "kiwi"]
+item = input("Enter a fruit: ")
+print(item, "is in the list:", item in fruits)
+print(item, "is not in the list:", item not in fruits)
+ 
+#OUTPUT 1
+#Enter a fruit: grapes
+#grapes is in the list: False
+#grapes is not in the list: True
+
+#OUTPUT 2
+#Enter a fruit: apple
+#apple is in the list: True
+#apple is not in the list: False
