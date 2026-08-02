2a981b9d04fafe510b910456e41814b438de9604
nani33-prog <nanifreefire33@gmail.com>
2026-08-02 12:31:40 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,16 @@
+First = "Nani Babu"
+Last = "Velagada"
+Full_name = First+" "+Last
+print(Full_name)
+print(Full_name.upper())
+print(Full_name.lower())
+print(Full_name.title())
+print("string length is ",len(Full_name))
+print(Full_name[0],Full_name[-1])
+
+#Nani Babu Velagada
+#NANI BABU VELAGADA
+#ani babu velagada
+#Nani Babu Velagada
+#string length is 18
+#N a
