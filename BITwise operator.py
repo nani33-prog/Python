03f7f047ce402d509ee19a1e6d77ca3e5e22b575
nani33-prog <nanifreefire33@gmail.com>
2026-08-02 13:54:01 +0530

    Add files via upload

---
+++ BITwise operator.py
@@ -0,0 +1,18 @@
+p = 12
+q = 10
+print(bin(p), bin(q))
+print(p & q)
+print(p | q)
+print(p ^ q)
+print(~p)
+print(p << 2)
+print(p >> 2)
+
+# OUT[UT
+# 0b1100 0b1010
+# 8
+# 14
+# 6
+#-13
+# 48
+$ 3
