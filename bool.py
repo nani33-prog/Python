d3f95ccaf54d8559ecf14098f604c0d18a7ea2dc
nani33-prog <nanifreefire33@gmail.com>
2026-08-02 13:10:47 +0530

    Add files via upload

---
+++ bool.py
@@ -0,0 +1,12 @@
+is_raining = True
+has_umbrella = False
+print(type(is_raining)) #<class 'bool'>
+print(is_raining & has_umbrella) #False
+print(is_raining | has_umbrella) #True
+print(not is_raining) #False
+
+@ OUTPUT
+# <class 'bool'>
+# False
+# True
+# False
