f20ce546e8341aed5638d057c09a240fc4e7c8bb
nani33-prog <nanifreefire33@gmail.com>
2026-08-02 13:40:50 +0530

    Add files via upload

---
+++ assignment operators.py
@@ -0,0 +1,35 @@
+
+score = 50
+print("Initial score (=):", score)
+
+score += 1
+print("After += 1:", score)
+
+score -= 1
+print("After -= 1:", score)
+
+score *= 1
+print("After *= 1:", score)
+
+score /= 1
+print("After /= 1:", score)
+
+score //= 1
+print("After //= 1:", score)
+
+score %= 1
+print("After %= 1:", score)
+
+score **= 1
+print("After **= 1:", score)
+
+
+# OUTPUT
+#Initial score (=): 50
+#After += 1: 51
+#After -= 1: 50
+#After *= 1: 50
+#After /= 1: 50.0
+#After //= 1: 50.0
+#After %= 1: 0.0
+#After **= 1: 0.0
