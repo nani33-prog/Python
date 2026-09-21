a6dae64d0af8e9b288932321a8cc3525cfb64f71
nani33-prog <nanifreefire33@gmail.com>
2026-09-21 12:12:12 +0530

    Add files via upload

---
+++ WEEK 4/4.A2.py
@@ -0,0 +1,10 @@
+list1 = [33,3.2,"nani",True,[3,8,9]]
+for element in list1:
+    print(element,type(element))
+
+#OUTPUT
+#33 <class 'int'>
+#3.2 <class 'float'>
+#nani <class 'str'>
+#True <class 'bool'>
+#[3, 8, 9] <class 'list'>
---
+++ WEEK 4/4.A3.py
@@ -0,0 +1,11 @@
+LIST1=[]
+LIST1.append(3)
+LIST1.append(8)
+LIST1.append(9)
+LIST1.append(5)
+LIST1.append(37)
+
+print(LIST1)
+
+#OUTPUT
+#[3, 8, 9, 5, 37]
