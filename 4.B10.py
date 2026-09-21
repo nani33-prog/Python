2d8fe57e31c3aff9769fec7a133fd2986720b62f
nani33-prog <nanifreefire33@gmail.com>
2026-09-21 12:57:35 +0530

    "Delete 4. files"

--- 4.B10.py
+++
@@ -1,5 +0,0 @@
-Tuple=(45,8,26,4)
-Tuple[3]=33
-
-#OUTPUT
-#'tuple' object does not support item assignment
--- 4.C3.py
+++
@@ -1,8 +0,0 @@
-Cinema = {"Rao Bahadhur":"Released", "Toxic":"Not Released"}
-print("Before updating:",Cinema)
-Cinema["Toxic"]="Released"
-print("After updating:",Cinema)
-
-#OUTPUT
-#Before updating: {'Rao Bahadhur': 'Released', 'Toxic': 'Not Released'}
-#After updating: {'Rao Bahadhur': 'Released', 'Toxic': 'Released'}
